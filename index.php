e1c1bed432c8e64cfb840b38cabbd2e24dec3d2c
Ngoc tan <tanhngcs17077@fpt.edu.vn>
2019-12-23 12:14:04 +0700

    update query

--- index.php
+++ index.php
@@ -12,27 +12,43 @@ port=5432 user=gpzpokwwiitgwz password=50c502f726bc61052e7337d64d1fe9b8bac3a783c
  
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. ");
+    die("ERROR: Could not connect.");
 }
  
 // Escape user inputs for security
-$id = mysqli_real_escape_string($link, $_REQUEST['id']);
-$name = mysqli_real_escape_string($link, $_REQUEST['name']);
-$cat = mysqli_real_escape_string($link, $_REQUEST['cat']);
-$date = mysqli_real_escape_string($link, $_REQUEST['date']);
-$price = mysqli_real_escape_string($link, $_REQUEST['price']);
-$description = mysqli_real_escape_string($link, $_REQUEST['desc']);
+$id = pg_escape_string ($link, $_REQUEST['id']);
+$name = pg_escape_string ($link, $_REQUEST['name']);
+$cat = pg_escape_string ($link, $_REQUEST['cat']);
+$date = pg_escape_string ($link, $_REQUEST['date']);
+$price = pg_escape_string ($link, $_REQUEST['price']);
+$description = pg_escape_string ($link, $_REQUEST['desc']);
+
+echo $id;
+echo "";
+echo $name;
+echo "";
+echo $cat;
+echo "";
+echo $date;
+echo "";
+echo $price;
+echo "";
+echo $desc;
+echo "";
 
 // Attempt insert query execution
-$sql = "INSERT INTO Product (Id, Product_Name, Catergory, Date, Price, Descriptions) VALUES ('$id', '$name', '$cat','$date','$price','abc')";
+$sql = "INSERT INTO Product (Id, Product_Name, Catergory, Date, Price, Descriptions) 
+VALUES ('$id', '$name', '$cat','$date','$price','abc')";
 echo $sql;
 
-$sql2 = "INSERT INTO Product (Id, Product_Name, Catergory, Date, Price, Descriptions) VALUES ('02', 'Me', 'CatX','2019-12-20',11,'abc')";
+$sql2 = "INSERT INTO Product (Id, Product_Name, Catergory, Date, Price, Descriptions) 
+VALUES ('02', 'Me', 'CatX','2019-12-20',11,'abc')";
 
 $sql3 = 'INSERT INTO public."Product" (
 "Date", "Id", "Product_Name", "Catergory", "Descriptions", "Price") VALUES ('."
-'2019-12-20'::date, '121210'::character varying(20), 'my product XYZ'::character varying(100), 'kit'::character varying(40), 'my product xyz'::character varying(200), '12'::integer)".
- 'returning "Id"';
+'2019-12-20'::date, '121210'::character varying(20), 'my product XYZ'::character varying(100), 
+'kit'::character varying(40), 'my product xyz'::character varying(200), '12'::integer)".
+'returning "Id"';
 echo $sql3;
 
 $result = pg_query($link, $sql3);
